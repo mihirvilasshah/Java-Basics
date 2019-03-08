96d208c956a6edf6c4261636006e38d095feec6b
Mihir <mihirhero2008@gmail.com>
2019-03-08 21:49:38 +0530

    protected accesss modifier

--- src/com/learn/java/accessmodifier/p1/ClassAlpha.java
+++ src/com/learn/java/accessmodifier/p1/ClassAlpha.java
@@ -4,4 +4,5 @@ public class ClassAlpha {
 	int d = 5;
 	public int pub = 10;
 	private int pri = 6;
+	protected int pro = 14;
 }
--- src/com/learn/java/accessmodifier/p1/ClassBeta.java
+++ src/com/learn/java/accessmodifier/p1/ClassBeta.java
@@ -1,8 +1,9 @@
 package com.learn.java.accessmodifier.p1;
 
-public class ClassBeta {
+public class ClassBeta extends ClassAlpha {
 	ClassAlpha classAlpha = new ClassAlpha();
 	public int betaPub = classAlpha.pub;
 //	private int betaPri = classAlpha.pri; // Cannot access private variable in other class
 	int betaDef = classAlpha.d; // Can access default variable in same package
+	int proFromAlpha = pro; // got the protected variable from parent class
 }
